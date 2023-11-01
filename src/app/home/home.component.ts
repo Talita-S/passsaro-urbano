ae35ab5c646ae86878ea85e98cadf4e57bf2f3fc
Talita-S <talitassantos2015@gmail.com>
2023-11-01 11:01:32 -0300

    feat: Binding the Offers service with the component Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { OffersService } from '../offers.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OffersService],
 })
 export class HomeComponent {
+  constructor(private offersService: OffersService) {}
 
+  ngOnInit() {
+    console.log(this.offersService.getOffers());
+  }
 }
